af85d65bc2eb3d95638193af506dd026f40c14d0
weeby-producer-user <user@weeby.producer>
2015-08-20 22:46:27 +0000

    Updated player's skills

--- src/ButtonRegister.js
+++ src/ButtonRegister.js
@@ -60,13 +60,18 @@ exports = {
 
   registerBombSkill: function(button, player, monster){
     var exist = false;
-    var damageBonus = 100;
-    //var skillDescription = scene.addText('Deal ' + damagebonus + '
+    var damageBonus = 90;
+    var skillDescription = scene.addText('Deal a lot of damage', {
+      superview: playerScrollView,
+      x: 40,
+      y: 150,
+      size: 28
+    });
     var unlockAt = scene.addText('Unlock at level 10', {
       superview: playerScrollView,
-      x: 50,
-      y: 170,
-      size: 30
+      x: 20,
+      y: 190,
+      size: 28
     });
     button.registerListener('onDown', function(){
       if(player.level >= 10){
@@ -81,6 +86,13 @@ exports = {
           player.bombDisplay.destroy();
           player.bombDisplay = DisplayText.display(player.smokeBombAmount, ' gold', 320, 170, 25, playerScrollView);
           damageBonus += 10;
+          skillDescription.destroy();
+          skillDescription = scene.addText('Deal tap damage x' + damageBonus, {
+            superview: playerScrollView,
+            x: 50,
+            y: 150,
+            size: 28
+          });
           if(!exist){
             var smokeBomb = GameUI.setUp(10, 820, 100, 100, 'resources/images/smokeBomb.png', scene.ui);
             smokeBomb.registerListener('onDown', function(){
@@ -114,16 +126,21 @@ exports = {
 
   registerInternalDeath: function(button, player, monster){
     var exist = false;
-    var damage = 100;
     var count = 0;
-    var perSecond = 500;
+    var multiplier = 0;
+    var perSecond = (1/multiplier)*1000;
     var duration = 30;
-    //var skillDescription = scene.addText('Deal ' + damagebonus + '
+    var skillDescription = scene.addText('Deal damage over time', {
+      superview: playerScrollView,
+      x: 55,
+      y: 250,
+      size: 28
+    });
     var unlockAt = scene.addText('Unlock at level 25', {
       superview: playerScrollView,
-      x: 50,
-      y: 270,
-      size: 30
+      x: 20,
+      y: 290,
+      size: 28
     });
     button.registerListener('onDown', function(){
       if(player.level >= 25){
@@ -135,15 +152,23 @@ exports = {
           player.totalGold = DisplayText.display(player.playerBank, ' gold', 320, 0, 25, playerScrollView);
           player.goldDisplay = DisplayText.display(player.playerBank, ' ', 150, 130, 35, scene.ui);
           player.internalDeathAmount += 100;
-          player.bombDisplay.destroy();
-          player.bombDisplay = DisplayText.display(player.internalDeathAmount, ' gold', 320, 270, 25, playerScrollView);
-          damage += 10;
+          player.internalDeathDisplay.destroy();
+          player.internalDeathDisplay = DisplayText.display(player.internalDeathAmount, ' gold', 320, 270, 25, playerScrollView);
+          multiplier += 3;
+          perSecond = (1/multiplier)*1000;
+          skillDescription.destroy();
+          skillDescription = scene.addText('Attack ' + multiplier + ' times/sec', {
+            superview: playerScrollView,
+            x: 25,
+            y: 270,
+            size: 28
+          });
           if(!exist){
             var internalDeath = GameUI.setUp(100, 820, 100, 100, 'resources/images/deathShadow.png', scene.ui);
             internalDeath.registerListener('onDown', function(){
               var clock = scene.addInterval(function(){
-                monster.target.hurt(damage);
-                DisplayText.tapDamage(damage);
+                monster.target.hurt(player.heroTapDamage);
+                DisplayText.tapDamage(player.heroTapDamage);
                 monster.displayHealth.destroy();
                 if(monster.bossTime){
                   monster.displayHealth = DisplayText.numSlashNum(monster.target.health.toFixed(1), monster.bossHealth, 130, 80, 'HP');
@@ -153,7 +178,7 @@ exports = {
                   monster.hpBar.setValue(monster.target.health/monster.monsterHealth, 100);
                 };
                 count += perSecond;
-                if(count/1000 == duration){
+                if(count/1000 >= duration){
                   scene.removeInterval(clock);
                 }
               }, perSecond);
